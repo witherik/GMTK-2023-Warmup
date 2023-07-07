62f76bcc00990798dc7f2b49c45245822dbf5b4a
Dasyel <dasyelwillems@gmail.com>
2023-07-07 18:48:23 +0200

    Code cleanup

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,38 +1,37 @@
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    [SerializeField] private Rigidbody _rigidbody;
+public class Movement : MonoBehaviour {
+    [SerializeField] private Rigidbody rigidBody;
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float accel = 25.0f;
     [SerializeField] private float drag = 10.0f;
-    private Vector3 inputVector;
-    private Vector3 lastVel;
+    private Vector3 _inputVector;
+    private Vector3 _lastVel;
 
-    void Update()
-    {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        inputVector = Vector3.ClampMagnitude(inputVector, 1);
+    private void Update() {
+        _inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        _inputVector = Vector3.ClampMagnitude(_inputVector, 1);
     }
-    void FixedUpdate()
-    {
-        Vector3 desiredVel = inputVector * moveSpeed;
 
-        Vector3 newVel = Vector3.MoveTowards(lastVel, Vector3.zero, drag * Time.fixedDeltaTime);
-        if (desiredVel != Vector3.zero)
-        {
+    private void FixedUpdate() {
+        var desiredVel = _inputVector * moveSpeed;
+
+        var newVel = Vector3.MoveTowards(_lastVel, Vector3.zero, drag * Time.fixedDeltaTime);
+        if (desiredVel != Vector3.zero) {
             newVel = Vector3.MoveTowards(newVel, desiredVel, accel * Time.fixedDeltaTime);
         }
-        newVel.y = _rigidbody.velocity.y;
-        Vector3 moveVelocity = newVel;
-        _rigidbody.velocity = moveVelocity;
-        lastVel = _rigidbody.velocity;
-        lastVel.y = 0;
+
+        var rigidBodyVelocity = rigidBody.velocity;
+        newVel.y = rigidBodyVelocity.y;
+        var moveVelocity = newVel;
+        rigidBody.velocity = moveVelocity;
+        _lastVel = rigidBodyVelocity;
+        _lastVel.y = 0;
     }
 
     void RotateToVelocity()
     {
-        Vector3 vel = _rigidbody.velocity;
+        var vel = rigidBody.velocity;
         vel.y = 0;
         // transform.localRotation = Vector3.RotateTowards()
     }
